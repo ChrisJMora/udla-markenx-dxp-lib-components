419a838f13354002e7f5d22058d9daf86a207136
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2025-10-04 01:12:37 -0500

    CONFIG PNJ 53 - Integración de Bootstrap y PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// PrimeNG módulos
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
@@ -10,6 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TableModule,
     AppRoutingModule
   ],
   providers: [],
